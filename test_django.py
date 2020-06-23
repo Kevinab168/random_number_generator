cec30d4a2f2178733a9d541a3d4128870422cfd9
Kevin <kevin.abraham1688@gmail.com>
2020-06-23 14:57:53 -0400

    Initial commit

---
+++ test_django.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def welcome():
+    return "welcome"
+
+
+@pytest.yield_fixture
+def driver():
+    # driver_options = Options()
+    # driver_options.add_argument("--no-sandbox")
+    # driver_options.add_argument("--disable-dev-shm-usage")
+    # driver_options.add_argument("--headless")
+    with webdriver.Remote(command_executor="127.0.0.1:9515") as driver:
+        yield driver
+
+
+def test_landing_page(driver, live_server, welcome):
+    driver.get(live_server.url)
+    assert welcome in driver.page_source
