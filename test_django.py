94bbff4d1bef358479b3c2129ab2b9d73d529f79
Kevin <kevin.abraham1688@gmail.com>
2020-06-26 11:52:06 -0400

    Add labels to form

--- test_django.py
+++ test_django.py
@@ -3,11 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture
-def welcome():
-    return "welcome"
-
-
 @pytest.yield_fixture
 def driver():
     driver_options = Options()
@@ -18,12 +13,21 @@ def driver():
         yield driver
 
 
-def test_landing_page(driver, live_server, welcome):
+def test_landing_page(driver, live_server):
     driver.get(live_server.url)
     # Find an input element
     user_input = driver.find_element_by_css_selector("input.user-guess")
     submit = driver.find_element_by_css_selector(".submit-btn")
     if user_input is not None and submit is not None:
+        assert True
+    else:
+        assert False
+
+
+def test_check_label(driver, live_server):
+    driver.get(live_server.url)
+    user_label = driver.find_element_by_css_selector('.user-label')
+    if user_label is not None:
         assert True
     else:
         assert False
