50a2d001973fac9b5c8a9026fe909cc5fd06a4e7
Kevin <kevin.abraham1688@gmail.com>
2020-06-29 13:44:27 -0400

    Refactor Tests
    - Add user_send_key and user_selects_between_1_and_10 fixtures
    - Add confttest.py

---
+++ conftest.py
@@ -0,0 +1,37 @@
+import os
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def user_selects_between_1_and_10(driver, live_server):
+    driver.get(live_server.url)
+    min_input = driver.find_element_by_css_selector('.min-level')
+    min_input.send_keys('1')
+    max_input = driver.find_element_by_css_selector('.max-level')
+    max_input.send_keys('10')
+
+
+@pytest.fixture
+def user_send_keys(driver, live_server):
+    def action(value):
+        user_input = driver.find_element_by_css_selector('.user-guess')
+        user_input.send_keys(value)
+        submit_button = driver.find_element_by_css_selector('.submit-btn')
+        submit_button.click()
+    return action
+
+
+@pytest.yield_fixture(scope='session')
+def driver():
+    if os.environ.get('CI'):
+        driver_options = Options()
+        driver_options.add_argument('--no-sandbox')
+        driver_options.add_argument('--disable-dev-shm-usage')
+        driver_options.add_argument('--headless')
+        driver = webdriver.Chrome(chrome_options=driver_options)
+    else:
+        driver = webdriver.Remote(command_executor='http://127.0.0.1:9515')
+    with driver:
+        yield driver
--- test_django.py
+++ test_django.py
@@ -1,93 +1,32 @@
-import os
-import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-
-@pytest.yield_fixture(scope='session')
-def driver():
-    if os.environ.get('CI'):
-        driver_options = Options()
-        driver_options.add_argument('--no-sandbox')
-        driver_options.add_argument('--disable-dev-shm-usage')
-        driver_options.add_argument('--headless')
-        driver = webdriver.Chrome(chrome_options=driver_options)
-    else:
-        driver = webdriver.Remote(command_executor='http://127.0.0.1:9515')
-    with driver:
-        yield driver
-
-
 def test_landing_page(driver, live_server):
     driver.get(live_server.url)
     # Find an input element
-    user_input = driver.find_element_by_css_selector('input.user-guess')
-    submit = driver.find_element_by_css_selector('.submit-btn')
-    if user_input is not None and submit is not None:
-        assert True
-    else:
-        assert False
+    assert driver.find_element_by_css_selector('input.user-guess')
+    assert driver.find_element_by_css_selector('.submit-btn')
+    assert driver.find_element_by_css_selector('.user-label')
+    assert driver.find_element_by_css_selector('.min-num-lab')
+    assert driver.find_element_by_css_selector('.max-num-lab')
 
 
-def test_check_label(driver, live_server):
-    driver.get(live_server.url)
-    user_label = driver.find_element_by_css_selector('.user-label')
-    min_label = driver.find_element_by_css_selector('.min-num-lab')
-    max_label = driver.find_element_by_css_selector('.max-num-lab')
-    if user_label is not None and min_label is not None and \
-       max_label is not None:
-        assert True
-    else:
-        assert False
-
-
-def test_guess_random_number(driver, live_server):
-    driver.get(live_server.url)
-    min_input = driver.find_element_by_css_selector('.min-level')
-    min_input.send_keys('1')
-    max_input = driver.find_element_by_css_selector('.max-level')
-    max_input.send_keys('10')
-    user_input = driver.find_element_by_css_selector('.user-guess')
-    user_input.send_keys('5')
-    submit_button = driver.find_element_by_css_selector('.submit-btn')
-    submit_button.click()
+def test_guess_random_number(driver, live_server, user_selects_between_1_and_10, user_send_keys):
+    user_send_keys('5')
     response_message = driver.find_element_by_css_selector('.response-message')
     response_message = response_message.text
     assert 'success' in response_message.lower() or \
         'sorry' in response_message.lower()
 
 
-def test_try_again(driver, live_server):
-    driver.get(live_server.url)
-    min_input = driver.find_element_by_css_selector('.min-level')
-    min_input.send_keys('1')
-    max_input = driver.find_element_by_css_selector('.max-level')
-    max_input.send_keys('10')
-    user_input = driver.find_element_by_css_selector('.user-guess')
-    user_input.send_keys('5')
-    submit_button = driver.find_element_by_css_selector('.submit-btn')
-    submit_button.click()
+def test_try_again(driver, live_server, user_selects_between_1_and_10, user_send_keys):
+    user_send_keys('5')
     # Find the try again button
     try_again_button = driver.find_element_by_css_selector('.try-again')
     try_again_button.click()
     # Check inputs again
-    user_label = driver.find_element_by_css_selector('.user-label')
-    if user_label is not None:
-        assert True
-    else:
-        assert False
+    assert driver.find_element_by_css_selector('.user-label')
 
 
-def test_limits(driver, live_server):
-    driver.get(live_server.url)
-    min_input = driver.find_element_by_css_selector('.min-level')
-    min_input.send_keys('1')
-    max_input = driver.find_element_by_css_selector('.max-level')
-    max_input.send_keys('10')
-    user_input = driver.find_element_by_css_selector('.user-guess')
-    user_input.send_keys('123')
-    submit_button = driver.find_element_by_css_selector('.submit-btn')
-    submit_button.click()
+def test_limits(driver, live_server, user_selects_between_1_and_10, user_send_keys):
+    user_send_keys('123')
     response_message = driver.find_element_by_css_selector('.response-message')
     assert 'choose another number' in response_message.text.lower()
 
